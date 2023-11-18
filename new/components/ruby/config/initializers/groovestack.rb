be8ab72fae3321807de395362f345713ea90bb04
Max Schridde <maxjschridde@gmail.com>
2023-11-17 19:54:00 -0800

    groovestack initializer improvements

--- new/components/ruby/config/initializers/groovestack.rb
+++ new/components/ruby/config/initializers/groovestack.rb
@@ -1,3 +1,5 @@
 # NOTE: to disable oauth, uncomment the line below
 
 # Core::Config::App.dynamic_config << { key: :oauth_enabled, build: Proc.new { false } }
+
+Core::Config::App.dynamic_config << { key: :hashed_ra_login_path, build: Proc.new { '#/login' } }
