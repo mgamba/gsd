9759630ec34367ab95d9f0f1fb263274d0f20316
Max Schridde <maxjschridde@gmail.com>
2023-11-20 10:59:41 -0800

    remove groovestack initializer cruft

--- new/components/ruby/config/initializers/groovestack.rb
+++ new/components/ruby/config/initializers/groovestack.rb
@@ -1,5 +1,1 @@
-# NOTE: to disable oauth, uncomment the line below
-
-# Core::Config::App.dynamic_config << { key: :oauth_enabled, build: Proc.new { false } }
-
-Core::Config::App.dynamic_config << { key: :hashed_ra_login_path, build: Proc.new { '#/login' } }
+# Custom groovestack initializer
